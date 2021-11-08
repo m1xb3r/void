813e3e6e54bc463a7e69bd69ea415c90b1c07c23
weberserker <77928890+weberserker@users.noreply.github.com>
2021-11-08 13:26:33 -0500

    Add files via upload
    Improved MAC Address randomness, Integrated Blacklisting and improved security when executing System Processes

--- void.py
+++ void.py
@@ -1,94 +1,66 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 #imported modules---------------------
+from os.path import basename, isfile
 import os
-from pyfiglet import Figlet
-from csv import reader
-import time 
+from subprocess import call, check_call, CalledProcessError, getoutput, Popen
+from sys import exit, stdout, stderr
+from time import sleep
 import random
 #-------------------------------------
-
 #Script starts and prints welcome text
-custom_txt = Figlet(font="future")
-
-print(custom_txt.renderText("Summoning the Void"))
-
+print("Summoning the Void")
 print("Your Darkest Secrets wii now be banished into the Void")
-
 #------------------------------------
-
-#MAC Add spoofing--------------------
-
-time.sleep(0.2)
-
+#MAC Add spoofing-------------------
 # Control flow that checks if the connection is working
-
-hostname = "5.9.95.167"
-
+hostname = "1.1.1.1"
 #The Parameter -c manages in a UNIX System the amount of packets sent
 # For Windows this param is -n
-
 response = None
-
-#start of the while loop
+#start of the while loop-------------------------------------------------------
 while response != 0:
-    os.system("ip link set dev eth0 down")
-
-#importing the MAC-vend.csv chosen as random for the first 24Bits.
+    Popen("ip link set dev eth0 down", shell= True )
+#importing the vendormac.txt chosen as random for the first 24Bits.
     lines = open("vendormac.txt").read().splitlines()
     myline = random.choice(lines)
-    
-    
+
     mac = myline
     fulladd = None
-    for number in range(16**6):
-        hex_num = hex(number)[2:].zfill(6)
-        fulladd ="{}{}{}:{}{}:{}{}".format(mac,*hex_num)
-        break
-
-
+    fulladd = mac+"%02x:%02x:%02x" % (random.randint(0, 255),random.randint(0, 255), random.randint(0, 255))
    # using the randomly chosen vendor octets, generating 3 random octets
+#Adding blacklist check
+    with open("blacklist.txt", "r") as blacklist:
+        if fulladd in blacklist.read():
+            print("Address is already blacklisted")
+            continue
 
-#sending the command to the shell
-    os.system("ip link set dev address " + fulladd)
+    Popen("ip link set dev address " + fulladd, shell= True )
+    Popen("ip link set dev  eth0 up", shell= True )
+    response = os.system("ping -c 4 " + hostname)
 
-#reactivating the NIC
-    os.system("ip link set dev  eth0 up")
-
-#checking the functionality of th enew generated address
-    response = os.system("ping -c 4 " + hostname) 
-    
-    if response == 0:   
+    if response == 0:
         print("Connection is working..")
         print("MAC Spoofing was successfull")
         print("Your new MAC Address is "+ fulladd)
         break
     else:
         print("Connection failed...")
-        print("Spoofing again") 
+        print("Blacklisting MAC Address")
+        with open("blacklist.txt", "a") as blacklist:
+            blacklist.write(fulladd+ "\n")
+        print("Spoofing again")
 # end of the while loop
-
-
-#end of MAC add spoofing------------------------
-
-
-
-time.sleep(1.0)
-
-os.system("sdmem -fllv")
-
-os.system("/etc/init.d/nscd restart")
-
-#os.system("anonsurf start")
-
-
-print("Insert your new Hostname below:")    
+#end of MAC add spoofing-------------------------------------------------------
+sleep(1.0)
+print("Insert your new Hostname below:")
 newhost= input("")
-
-
-os.system("hostname " + newhost)
-
+Popen("hostname " + newhost, shell= True)
 print("You have a new identity")
+os.system("sdmem -fllv")
+#call("anonsurf start")
+# TOR routing start----------------------------
+print(newhost)
 
+# TOR routing end----------------------------
 print("Your secrets will never see the sunlight again...")
-
